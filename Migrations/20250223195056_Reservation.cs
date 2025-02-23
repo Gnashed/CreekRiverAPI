a76742a71879ee8625c208ac4537d29fd8c9fb5b
Tion Blackmon <tion.blackmon@outlook.com>
2025-02-23 13:54:46 -0600

    Had to reapply migrations since I made several changes to my EF Core models, specifically the dates in the Reservation entity.

---
+++ Migrations/20250223195056_Reservation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CreekRiver.Migrations
+{
+    /// <inheritdoc />
+    public partial class Reservation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CheckinDate", "CheckoutDate" },
+                values: new object[] { new DateTime(2024, 6, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 6, 20, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CheckinDate", "CheckoutDate" },
+                values: new object[] { new DateTime(2024, 8, 24, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 8, 26, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CheckinDate", "CheckoutDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CheckinDate", "CheckoutDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+    }
+}
